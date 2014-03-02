b9a52941c93c48ad139bd99068a5c40f512f5ebd
Kelly Gendron <kelly@404htm.com>
2014-03-01 20:21:41 -0500

    Initial Project Structure - Empty

---
+++ Web404/Web404.Site.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Web404.Site.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
