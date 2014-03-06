1bee73df0335b648d7c10983398eee480efc6c94
Kelly Gendron <github@404htm.com>
2014-03-06 00:00:36 -0500

    Fixed connection strings for local database
    Related Work Items: #45, #46

--- Web404/Web404.CMS/Context.cs
+++ Web404/Web404.CMS/Context.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Infrastructure; 
 
@@ -12,6 +11,11 @@ namespace Web404.CMS
 {
     public class Context : DbContext
     {
+        internal Context(string connectionString)
+            : base(connectionString)
+        {
+
+        }
         public DbSet<Page> Pages { get; set; }
         public DbSet<Tag> Tags { get; set; }
     }
