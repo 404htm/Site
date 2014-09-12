61c290c59a6dc6ca8e3de8a0e361f845578c41ea
github@404htm.com <github@404htm.com>
2014-09-12 00:07:34 -0400

    Scaffolding classes for azure conversion

---
+++ Web404/Web404.CMS/AzureDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web404.CMS
+{
+	public class AzureDataManager
+	{
+		 string _cnn;
+		const int DEFAULT_PAGE_COUNT = 15;
+
+
+        public AzureDataManager(string connectionString)
+        {
+            _cnn = connectionString;
+        }
+
+
+	}
+}
