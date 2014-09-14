9b8233c19b6bba8b02d716d9d2451a27338c1ba3
github@404htm.com <github@404htm.com>
2014-09-13 22:38:43 -0400

    Changes to Post Object #10

--- Web404/Web404.AzureCMS/PostEntity.cs
+++ Web404/Web404.AzureCMS/PostEntity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Web404.AzureCMS
@@ -13,13 +14,15 @@ namespace Web404.AzureCMS
 			public PostEntity(string URLTitle, DateTime date, PostType type)
 			{
 				PartitionKey= "*";
-				RowKey = "Key";
+				RowKey = URLTitle;
 			}
 
 			public PostType Type { get; set; }
+			public DateTime Date { get; private set; }
 			public bool Active { get; set; }
 			public string Title { get; set; }
-			public DateTime Date { get; private set; }
+			public string Summary { get; set; }
+			public string Tags { get; set;}
 
 
 
