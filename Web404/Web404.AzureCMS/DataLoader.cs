5b28d9e41e6914428c164496374431714f563abf
github@404htm.com <github@404htm.com>
2014-09-15 22:59:11 -0400

    Fixed Test with development account #11

---
+++ Web404/Web404.AzureCMS/DataLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Web404.AzureCMS
+{
+	public class AzureDataLoader
+	{
+		string _cnn;
+
+		public AzureDataLoader(string cnn)
+		{
+			_cnn = cnn;
+		}
+
+		public void SavePost(PostEntity post)
+		{
+
+			CloudStorageAccount storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+			CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+			CloudTable table = tableClient.GetTableReference("posts");
+			TableOperation insertOperation = TableOperation.InsertOrReplace(post);
+
+			table.Execute(insertOperation); 
+
+		}
+	}
+}
---
+++ Web404/Web404.Uploader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web404.AzureCMS;
+
+namespace Web404.Uploader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var directory = @"C:\Users\Kelly Gendron\Source\Repos\BlogPosts\Articles\Home\IEWhiteScreen";
+			var uploader = new AzureDataLoader("");
+			var parser = new PostParser(uploader);
+			parser.UploadDirectory(directory);
+		}
+	}
+}
