ee74a9e380caed16392d5a3f43f9d2ba2f9252a7
github@404htm.com <github@404htm.com>
2014-09-15 21:08:20 -0400

    Added starter project for Azure upload #11

---
+++ Web404/Web404.Uploader/PostParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web404.AzureCMS;
+
+namespace Web404.Uploader
+{
+	public class PostParser
+	{
+		AzureDataLoader _uploader;
+		public PostParser(AzureDataLoader uploader)
+		{
+			_uploader = uploader;
+		}
+
+		public void UploadDirectory(string directory)
+		{
+			var dir = new DirectoryInfo(directory);
+			var files = dir.EnumerateFiles();
+
+			foreach(var f in files)
+			{
+				if(string.Compare(f.Name, "post.html", true) == 0)
+				{
+					var post  = new PostEntity("test", DateTime.Now, PostType.Article);
+
+
+					_uploader.SavePost(post);
+				}
+				else
+				{
+
+				}
+			}
+
+		}
+
+	}
+}
