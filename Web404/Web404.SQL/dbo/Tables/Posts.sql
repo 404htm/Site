bb549d95d96d0004b1558386eba7559133833ab6
github@404htm.com <github@404htm.com>
2014-10-05 00:42:30 -0400

    Added Year Column / Index #19

--- Web404/Web404.SQL/dbo/Tables/Posts.sql
+++ Web404/Web404.SQL/dbo/Tables/Posts.sql
@@ -3,11 +3,17 @@
     [Date]        DATETIME       NOT NULL,
     [Title]       VARCHAR (100)  NULL,
     [URLName]     VARCHAR (100)  NULL,
+	[Year]        CHAR(4) NULL,
     [Summary]     NVARCHAR (MAX) NULL,
 	[IsMainPage]  Bit NOT NULL,
     [Active]      BIT            NOT NULL,
     [SectionID] INT NULL , 
     CONSTRAINT [PK_Posts_1] PRIMARY KEY CLUSTERED ([ID] ASC), 
-    CONSTRAINT [FK_Posts_Section] FOREIGN KEY ([SectionID]) REFERENCES [Sections]([ID])
+    CONSTRAINT [FK_Posts_Section] FOREIGN KEY ([SectionID]) REFERENCES [Sections]([ID]),
+
 );
 
+
+GO
+
+CREATE UNIQUE INDEX [UX_Posts_Url] ON [dbo].[Posts] ([Year], [URLName])
