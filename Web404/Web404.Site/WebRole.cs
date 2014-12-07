d662cebe1498506f698b4fb158e441d7901ac783
github@404htm.com <github@404htm.com>
2014-12-07 13:50:03 -0500

    Trying once again to keep azure from unloading the webservice every 20 minutes. Why can't I punch a cloud, I want to punch a cloud.

--- Web404/Web404.Site/WebRole.cs
+++ Web404/Web404.Site/WebRole.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Web.Administration;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
@@ -9,6 +10,23 @@ namespace Web404.Site
 {
     public class WebRole : RoleEntryPoint
     {
+
+		public override void Run()
+		{
+			using (var serverManager = new ServerManager())
+			{
+				var mainSite = serverManager.Sites[RoleEnvironment.CurrentRoleInstance.Id + "_Web"];
+				var mainApplication = mainSite.Applications["/"];
+				var mainApplicationPool = serverManager.ApplicationPools[mainApplication.ApplicationPoolName];
+				mainApplicationPool["autoStart"] = true;
+				mainApplicationPool["startMode"] = "AlwaysRunning";
+
+				serverManager.CommitChanges();
+			}
+
+			base.Run();
+		}
+
         public override bool OnStart()
         {
             // For information on handling configuration changes
